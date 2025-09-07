cf840d6dc013e8ee8ad9d740604487a6839c79d3
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2025-09-07 23:56:53 +0100

    Fix: Re-applied views as module import in urls.py for linter compliance

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register, admin_view, librarian_view, member_view, add_book, edit_book, delete_book
+from . import views
 from django.contrib.auth import views as auth_views
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    path('books/', list_books, name='book_list'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', register, name='register'),
+    path('books/', views.list_books, name='book_list'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('admin_dashboard/', admin_view, name='admin_view'),
-    path('librarian_dashboard/', librarian_view, name='librarian_view'),
-    path('member_dashboard/', member_view, name='member_view'),
-    path('book/add/', add_book, name='add_book'),
-    path('book/<int:pk>/edit/', edit_book, name='edit_book'),
-    path('book/<int:pk>/delete/', delete_book, name='delete_book'),
+    path('admin_dashboard/', views.admin_view, name='admin_view'),
+    path('librarian_dashboard/', views.librarian_view, name='librarian_view'),
+    path('member_dashboard/', views.member_view, name='member_view'),
+    path('book/add/', views.add_book, name='add_book'),
+    path('book/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
 ]
