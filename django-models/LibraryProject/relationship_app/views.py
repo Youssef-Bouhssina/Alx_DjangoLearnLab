2e0bdcbb52c8ade306d782db0ea018c157c576cf
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2025-09-07 23:37:11 +0100

    Implement user authentication (login, logout, registration)

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            return redirect('relationship_app:login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 
 def list_books(request):
     books = Book.objects.all()
