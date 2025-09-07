d866f5cbf34f3f8beac8905a53403f3d86555733
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2025-09-07 23:32:57 +0100

    Fix: Explicitly imported views in relationship_app/urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from .views import book_list, LibraryDetailView
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    path('books/', views.book_list, name='book_list'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', book_list, name='book_list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
