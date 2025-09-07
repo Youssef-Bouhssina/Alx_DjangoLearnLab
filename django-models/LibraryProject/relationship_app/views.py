e6ce915f856dc25d065a194c69773e31e7570cb2
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2025-09-07 23:38:26 +0100

    Fix: Added missing authentication imports to views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
