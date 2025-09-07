5e5c5b99b16822642a2a7f40639f1be2bfc36f08
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2025-09-07 23:39:54 +0100

    Revert: Explicit imports in relationship_app/urls.py for linter compliance

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView, register
 from django.contrib.auth import views as auth_views
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    path('books/', views.list_books, name='book_list'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.register, name='register'),
+    path('books/', list_books, name='book_list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
