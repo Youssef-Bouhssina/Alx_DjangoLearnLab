44826b5d08e5a7cbc764f52fd6111ab53e61bd04
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2025-09-14 04:52:44 +0100

    Implement custom user model in Django

---
+++ advanced_features_and_security/users/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', 'date_of_birth', 'profile_photo', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + ( 
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email', 'date_of_birth', 'profile_photo')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
