34198af389580bd6fcf49996c573a918ac5183c3
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2025-09-14 05:36:59 +0100

    Add `ExampleForm` for demonstration and update `.idea` configuration files for SDK setup

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -10,3 +10,15 @@ class BookForm(forms.ModelForm):
             'author': forms.TextInput(attrs={'class': 'form-control'}),
             'publication_year': forms.NumberInput(attrs={'class': 'form-control'}),
         }
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+
+    def clean_email(self):
+        # Example of secure email validation
+        email = self.cleaned_data.get('email')
+        if not email.endswith(('.com', '.org', '.edu', '.net')):
+            raise forms.ValidationError('Please enter a valid email domain')
+        return email
