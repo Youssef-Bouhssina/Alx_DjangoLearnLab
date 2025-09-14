551a097c59c431c8ba8003ef18e03196e4d3c7a6
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2025-09-14 05:48:26 +0100

    Add media file settings and enhance CustomUser model with profile photo and date of birth fields
    Includes:
    - Configured `STATIC_ROOT` and `MEDIA_ROOT` for static and media file handling.
    - Added `.gitkeep` to ensure `profile_photos` directory is included in version control.
    - Updated `CustomUser` to include `profile_photo` and `date_of_birth` fields.
    - Implemented `CustomUserManager` for user and superuser creation with custom validations.

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -134,6 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Media files (Uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Media files (User uploaded files)
 MEDIA_URL = '/media/'
